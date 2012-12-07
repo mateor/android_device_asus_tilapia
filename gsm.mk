763c1138f1ba274e17c59666059d97c19a6b8f83
Paul Beeler <sparksco@gmail.com>
2012-12-07 16:41:20 -0700

    Add GSM bits
    Move ta.mk to gsm.mk and remove teamandroid copywrite header

---
+++ gsm.mk
@@ -0,0 +1,11 @@
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+
+# CyanogenMod's GSM APN list
+PRODUCT_COPY_FILES += \
+    device/asus/tilapia/system/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# CyanogenMod's GSM SPN overrides list
+PRODUCT_COPY_FILES += \
+    device/asus/tilapia/system/etc/spn-conf.xml:system/etc/spn-conf.xml
